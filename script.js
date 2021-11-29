edd82243ad170d78b6700598c0165282c5289a5d
Jergie Mascan <jiji.mascan@gmail.com>
2021-11-29 21:56:30 +0100

    changes in functionality and search input

--- script.js
+++ script.js
@@ -11,13 +11,10 @@ var requestOptions = {
   headers: headers,
   redirect: "follow",
 };
-
+/********************************************************************/
 const tableCountries = document.getElementById("table-country");
-const btnSearch = document.getElementById("btn-search");
-let input = document.getElementById("search-country");
-// generated from function
-let btnDetails = document.getElementsByClassName("btn-details");
-let btnStates = document.getElementsByClassName("btn-states");
+let allCountries = [];
+let globalISO = "";
 
 async function getAllCountries() {
   try {
@@ -26,44 +23,45 @@ async function getAllCountries() {
       requestOptions
     );
     const countries = await response.json();
-
+    // console.log(countries);
+    allCountries = countries;
     for (let country of countries) {
       tableCountries.innerHTML += `
         <td>
           <li class="country-list">${country.name} ${country.iso2}
-            <button class="btn-details" id=${country.iso2} value=${country}>Details</button>
-            <button class="btn-states" id=${country.iso2}>States</button>
+            <button onClick="getCountryDetails(event)"  id=${country.iso2}>Details</button>
+            <button onClick="getCountryStates(event)" id=${country.iso2}>States</button>
           </li>
         </td>
-        `;
+      `;
     }
-
-    searchCountry(countries);
-    getCountryDetails(btnDetails);
-    getCountryStates(btnStates);
   } catch (error) {
     console.log(error);
   }
 }
 getAllCountries();
 
-function getCountryDetails(btnDetails) {
-  for (let i = 0; i < btnDetails.length; i++) {
-    btnDetails[i].addEventListener("click", async function (e) {
-      let iso2 = e.target.id;
-      // console.log(iso2);
-      const responseCountryDetails = await fetch(
-        `https://api.countrystatecity.in/v1/countries/${iso2}`,
-        requestOptions
-      );
-      const countryDetails = await responseCountryDetails.json();
-      console.log(countryDetails);
-      console.log(countryDetails.capital);
+async function getCountryDetails(event) {
+  try {
+    let iso2 = event.target.id;
+    // console.log(iso2);
+    if (globalISO !== iso2) {
+      document.getElementById("table-country-states").innerHTML = "";
+    }
 
-      const tableDetails = document.getElementById("table-country-details");
-      tableDetails.innerHTML = "";
-      // tableDetails.append(countryDetails);
-      tableDetails.innerHTML = `
+    globalISO = iso2;
+    const responseCountryDetails = await fetch(
+      `https://api.countrystatecity.in/v1/countries/${iso2}`,
+      requestOptions
+    );
+    const countryDetails = await responseCountryDetails.json();
+    // console.log(countryDetails);
+
+    const tableDetails = document.getElementById("table-country-details");
+    tableDetails.innerHTML = "";
+    // tableDetails.append(countryDetails);
+    tableDetails.innerHTML = `
+    <td class="details-list">
       <li>Native: ${countryDetails.native}</li>
       <li>Name: ${countryDetails.name}</li>
       <li>Numeric_code: ${countryDetails.numeric_code}</li>
@@ -72,51 +70,65 @@ function getCountryDetails(btnDetails) {
       <li>Region: ${countryDetails.region}</li>
       <li>Currency: ${countryDetails.currency}</li>
       <li>Subregion: ${countryDetails.subregion}</li>
-      
-      `;
-    });
+    </td>
+  `;
+  } catch (error) {
+    console.log(error);
   }
 }
 
-function getCountryStates(btnStates) {
-  for (let i = 0; i < btnStates.length; i++) {
-    btnStates[i].addEventListener("click", async function (e) {
-      let iso2 = e.target.id;
-      console.log(iso2);
+async function getCountryStates(event) {
+  try {
+    let iso2 = event.target.id;
+    // console.log(iso2);
+    if (globalISO !== iso2) {
+      document.getElementById("table-country-details").innerHTML = "";
+    }
+    globalISO = iso2;
 
-      const response2 = await fetch(
-        `https://api.countrystatecity.in/v1/countries/${iso2}/states`,
-        requestOptions
-      );
-      let tableStates = document.getElementById("table-country-states");
-      const countryStatesData = await response2.json();
-      // console.log(countryStatesData);
-      if (countryStatesData.length === 0) {
-        tableStates.innerHTML = `<p class="errorMessage">No States found</p>`;
-      } else {
-        tableStates.innerHTML = "";
-        for (let state of countryStatesData) {
-          tableStates.innerHTML += `<li class="state-list">${state.name} ${state.iso2}</li>`;
-        }
+    const response2 = await fetch(
+      `https://api.countrystatecity.in/v1/countries/${iso2}/states`,
+      requestOptions
+    );
+    let tableStates = document.getElementById("table-country-states");
+    const countryStatesData = await response2.json();
+    // console.log(countryStatesData);
+    if (countryStatesData.length === 0) {
+      tableStates.innerHTML = `<p class="errorMessage">No States found</p>`;
+    } else {
+      tableStates.innerHTML = "";
+      for (let state of countryStatesData) {
+        tableStates.innerHTML += `<li class="state-list">${state.name} ${state.iso2}</li>`;
       }
-    });
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
-function searchCountry(countries) {
-  btnSearch.addEventListener("click", function () {
-    const searchCountry = countries.filter((count) => {
-      if (input.value === count.name) {
-        tableCountries.innerHTML = `
-        <td>
-          <li class="country-list">${count.name} ${count.iso2}
-            <button class="btn-details" id=${count.iso2} value=${count}>Details</button>
-            <button class="btn-states" id=${count.iso2}>States</button>
-          </li>
-        </td> `;
-        getCountryDetails(btnDetails);
-        getCountryStates(btnStates);
+let input = document.getElementById("search-country");
+function searchCountry(event) {
+  try {
+    let inputValue = event.target.value;
+    tableCountries.innerHTML = "";
+
+    for (let country of allCountries) {
+      if (country.name.includes(capitalizeFirstLetter(inputValue))) {
+        tableCountries.innerHTML += `
+    <td>
+      <li class="country-list">${country.name} ${country.iso2}
+        <button onClick="getCountryDetails(event)"  id=${country.iso2}>Details</button>
+        <button onClick="getCountryStates(event)" id=${country.iso2}>States</button>
+      </li>
+    </td>
+    `;
       }
-    });
-  });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
